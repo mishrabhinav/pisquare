d437c27cb837d70f93ebee7dd81796fdab25a4de
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-06-08 14:09:24 +0100

    Implement hardware random number generator driver.
    Make use of the hardware generator on the Pi SoC.

---
+++ libarmc/rpi-random.c
@@ -0,0 +1,20 @@
+#include "rpi-random.h"
+
+#include <stdint.h>
+
+static int random_initialised = 0;
+
+void RPI_InitRandom(void)
+{
+	random_initialised = 1;
+
+	*(volatile uint32_t *)RPI_HW_RNG_STATUS = RNG_WARMUP_COUNT;
+	*(volatile uint32_t *)RPI_HW_RNG_CTRL = RPI_HW_RNG_CTRL_EN;
+}
+
+uint32_t RPI_GetRandom(void)
+{
+	while ((*(volatile uint32_t *)RPI_HW_RNG_STATUS >> 24) == 0);
+
+	return (uint32_t)(*(volatile uint32_t *)RPI_HW_RNG_DATA);
+}
